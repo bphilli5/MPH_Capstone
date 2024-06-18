7bd9aba6ef0949cb8a4d78a0c840770a3850ebcb
BPhillips <brendanphillips94@gmail.com>
2024-06-18 10:29:53 -0400

    Reformating

--- bphillips_capstone_setup.r
+++ bphillips_capstone_setup.r
@@ -3,12 +3,11 @@
 #> Data ingestion and libraries
 ###############################################################################
 
-library(tidyverse)
 library(haven)
 library(car)
 library(broom)
 library(ggplot2)
+library(tidyverse)
 
 # Reading data files
-# setwd("S:/HMG-Capstone-Phillips/Data")
 d_sas <- read_sas("S:/HMG-Capstone-Phillips/Data/dedup_pt_encounters_eci_analysis.sas7bdat")
