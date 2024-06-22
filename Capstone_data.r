5cbe0fd6affb710d01c97649909802e478e96917
Brendan Phillips <brendanphillips94@gmail.com>
2024-06-22 13:27:53 -0400

    Cleanup

--- Capstone_data.r
+++
@@ -1,9 +0,0 @@
-#> Capstone Project
-#> Brendan Phillips
-
-library(tidyverse)
-library(haven)
-
-# Reading data files
-setwd("S:/HMG-Capstone-Phillips/Data")
-d <- read_sas("dedup_pt_encounters_eci_analysis.sas7bdat")
